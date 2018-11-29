f07a6730f6aec77125972f78b8f318d571149efe
jadnson <jadnson@umich.edu>
2018-11-29 06:34:07 -0500

    Almost Done

---
+++ app/src/main/java/com/example/jpier/jessicaalexahw2/InventoryCheck.java
@@ -0,0 +1,30 @@
+package com.example.jpier.jessicaalexahw2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InventoryCheck extends Activity implements View.OnClickListener {
+
+    Button buttonHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_check);
+
+        buttonHome = (Button) findViewById(R.id.buttonHome);
+
+        buttonHome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonHome) {
+            Intent intentHome = new Intent(this, WelcomeScreen.class);
+            startActivity(intentHome);
+        }
+    }
+}
